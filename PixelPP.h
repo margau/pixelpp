62547d2834600b7dce26ec03e71401204c2b9987
Marvin Gaube <dev@marvingaube.de>
2019-10-21 16:53:43 +0200

    Add .gitignore

--- PixelPP.h
+++ PixelPP.h
@@ -1,6 +1,13 @@
 #ifndef PIXELPP_H
 #define PIXELPP_H
 
+#if (ARDUINO >= 100)
+ #include <Arduino.h>
+#else
+ #include <WProgram.h>
+ #include <pins_arduino.h>
+#endif
+
 #define COLOR_MONO 0
 #define COLOR_RGB 1
 #define COLOR_RGBW 2
@@ -12,6 +19,7 @@ class PixelPP {
  public:
 
   // Constructor: number of LEDs, ColorType
+  PixelPP(void);
   PixelPP(uint16_t n, colorType t=COLOR_RGB);
   ~PixelPP();
 
