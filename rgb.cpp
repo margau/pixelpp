2f4ac768e1b0eb9d61bca4a25d65ece151d21278
Marvin Gaube <dev@marvingaube.de>
2019-10-28 16:35:38 +0100

    Remove RGB class

--- rgb.cpp
+++
@@ -1,6 +0,0 @@
-#include "rgb.h"
-
-RGB::RGB(uint8_t r, uint8_t g, uint8_t b) : red(r), green(g), blue(b)
-{
-
-}
--- rgb.h
+++ rgb.h
@@ -9,15 +9,6 @@ typedef struct rgb {
   uint8_t blue;
 } rgb;
 
-class RGB {
-public:
-    uint8_t red;
-    uint8_t green;
-    uint8_t blue;
-    RGB();
-    RGB(uint8_t r, uint8_t g, uint8_t b);
-};
-
 typedef struct hsv {
     uint8_t hue;
     uint8_t sat;
