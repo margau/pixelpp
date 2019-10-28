6e3d3735b124231bca5d6810274459ccb06568ea
Marvin Gaube <dev@marvingaube.de>
2019-10-28 17:35:07 +0100

    Prepare Period

--- SineEffect.cpp
+++ SineEffect.cpp
@@ -12,12 +12,11 @@ SineEffect::~SineEffect()
 
 void SineEffect::render(unsigned long t)
 {
-    uint16_t sin2 = (uint16_t)sinu8(t*(INT16_MAX/_peri));
-    Serial.println(sin2);
-    for (uint8_t i = 0; i < _parent->getNumLeds(); i++)
+    uint16_t numPixels = _parent->getNumLeds();
+    for (uint16_t i = 0; i < _parent->getNumLeds(); i++)
     {
         rgb dat;
-        uint16_t sin = (uint16_t)sinu8(t*(INT16_MAX/_peri));
+        uint16_t sin = (uint16_t)sinu8(t*(INT16_MAX/_peri)+(i % _width)*INT16_MAX);
         dat.red = (dat.red * sin) >> 8U;
         dat.green = (dat.green * sin) >> 8U;
         dat.blue = (dat.blue * sin) >> 8U;
