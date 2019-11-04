01965b9aaffcaba0a9077f58687cf6a31b32237f
Marvin Gaube <dev@marvingaube.de>
2019-11-04 18:37:55 +0100

    Add Basics of PixEffect

---
+++ PixEffect.cpp
@@ -0,0 +1,42 @@
+#include "PixEffect.hpp"
+
+PixEffect::PixEffect(PixelPP* parent, rgb color, uint16_t length, uint8_t num) : _length(length), _num(num), _color(color), Effect(parent)
+{
+        randomSeed(analogRead(0));
+}
+
+PixEffect::~PixEffect()
+{
+}
+
+void PixEffect::render(unsigned long t)
+{
+        for(int i=0; i<_num; i++) {
+                // Add new random pixel
+                pixfade p;
+                p.n = random(_parent->getNumLeds());
+                p.end = t+_length;
+                pixfadeVector.push_back(p);
+        }
+
+        // Iterate and display
+        auto it = pixfadeVector.begin();
+        while(it != pixfadeVector.end()) {
+                // Remove element if end is over
+                if(it->end <= t) {
+                        it = pixfadeVector.erase(it);
+                } else {
+                        // Display pixel and move on
+                        //uint16_t factor = (INT16_MAX*(it->end-t))/_length;
+                        uint16_t factor = INT16_MAX;
+                        rgb& pix = _parent->_leds_rgb[it->n];
+                        pix=_color;
+
+                        // Calculate Fade factor
+                        pix.red = (pix.red * factor) >> 8U;
+                        pix.green = (pix.green * factor) >> 8U;
+                        pix.blue = (pix.blue * factor) >> 8U;
+                        ++it;
+                }
+        }
+}
---
+++ PixEffect.hpp
@@ -0,0 +1,25 @@
+#ifndef PixEFFECT_HPP
+#define PixEFFECT_HPP
+#include "common.h"
+#include "rgb.h"
+#include "PixelPP.h"
+#include "Effect.hpp"
+
+struct pixfade {
+  uint16_t n;
+  unsigned long end;
+};
+
+class PixEffect : public Effect
+{
+private:
+    rgb _color;
+    std::vector<pixfade> pixfadeVector;
+    uint16_t _length;
+    uint8_t _num;
+public:
+    PixEffect(PixelPP* parent, rgb color, uint16_t length, uint8_t num);
+    ~PixEffect();
+    void render(unsigned long t);
+};
+#endif
