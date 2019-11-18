9913cf185dda132eae8b8a22698ca79b591661dd
autinerd <sidneyjohn23@kuyateh.eu>
2019-11-18 17:48:51 +0100

    add dim effect

---
+++ DimEffect.cpp
@@ -0,0 +1,14 @@
+#include "DimEffect.hpp"
+
+DimEffect::DimEffect(PixelPP* parent, uint8_t percentage) : _percentage(percentage), Effect(parent)
+{
+}
+
+DimEffect::~DimEffect()
+{
+}
+
+void DimEffect::render(uint32_t t)
+{
+
+}
---
+++ DimEffect.hpp
@@ -0,0 +1,17 @@
+#ifndef DIMEFFECT_HPP
+#define DIMEFFECT_HPP
+#include "common.h"
+#include "rgb.h"
+#include "PixelPP.hpp"
+#include "Effect.hpp"
+
+class DimEffect : public Effect
+{
+private:
+    uint8_t _percentage; // 0 = no dimming, 255 = full dimming
+public:
+    DimEffect(PixelPP* parent, uint8_t percentage);
+    ~DimEffect();
+    void render(uint32_t t);
+};
+#endif
