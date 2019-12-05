417fcaef9699098b104a05bde3d3f620614db70d
autinerd <sidneyjohn23@kuyateh.eu>
2019-12-05 16:08:51 +0100

    pix effect

--- PixEffect.cpp
+++ PixEffect.cpp
@@ -11,32 +11,36 @@ PixEffect::~PixEffect()
 
 void PixEffect::render(unsigned long t)
 {
-        // Add additional pixels if some timed out
-        for (int i = 0; i < _num - pixfadeVector.size(); i++)
-        {
-                pixfadeVector.push_back({
-                    random(_parent->getNumLeds()),
-                    t + _length
-                });
-        }
+	static uint16_t lastadd = 0;
 
-        // Iterate and display
-        auto it = pixfadeVector.begin();
-        while(it != pixfadeVector.end()) {
-                // Remove element if end is over
-                if(it->end <= t) {
-                        it = pixfadeVector.erase(it);
-                } else {
-                        // Display pixel and move on
-                        uint8_t factor = (INT8_MAX*(it->end - t)) / _length;
-                        rgb& pix = _parent->_leds_rgb[it->n];
-                        pix = _color;
+	if (t - lastadd >= _length / _num)
+	{
+		pixfadeVector.push_back({
+			random(_parent->getNumLeds()),
+			t + _length
+		});
+		lastadd = t;
+	}
 
-                        // Calculate Fade factor
-                        pix.red = (_color.red * factor) >> 8U;
-                        pix.green = (_color.green * factor) >> 8U;
-                        pix.blue = (_color.blue * factor) >> 8U;
-                        ++it;
-                }
-        }
+	// Iterate and display
+	std::list<pixfade>::iterator it = pixfadeVector.begin();
+	while (it != pixfadeVector.end()) {
+		// Remove element if end is over
+		if (it->end <= t)
+		{
+			it = pixfadeVector.erase(it);
+		}
+		else
+		{
+			// Display pixel and move on
+			uint8_t factor = (INT8_MAX*(it->end - t)) / _length;
+			rgb& pix = _parent->_leds_rgb[it->n];
+			pix = {
+				(_color.red * factor) >> 8U,
+				(_color.green * factor) >> 8U,
+				(_color.blue * factor) >> 8U
+			};
+			++it;
+		}
+	}
 }
